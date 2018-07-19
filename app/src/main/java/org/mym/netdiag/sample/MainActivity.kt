ef863d84627ff07f29e35e9f528fe5ba4578ccac
Muyangmin <muyangmin@foxmail.com>
2018-07-19 14:58:58 +0800

    Add sample with a mock task.

--- app/src/main/java/org/mym/netdiag/sample/MainActivity.kt
+++ app/src/main/java/org/mym/netdiag/sample/MainActivity.kt
@@ -1,12 +1,34 @@
 package org.mym.netdiag.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.mym.kotlog.GlobalTagDecorator
+import org.mym.kotlog.L
+import org.mym.netdiag.NetworkDiagnosis
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(L) {
+            install()
+            addDecorator(GlobalTagDecorator("Sample"))
+        }
+
+        with(NetworkDiagnosis) {
+            debug = true
+            logger = { L.d(msg = it) }
+            val pool = Executors.newFixedThreadPool(5)
+            executor = {
+                pool.submit(it)
+            }
+        }
+
+        NetworkDiagnosis.execute(FakeTask()) {
+            L.i("FakeTask result: $it")
+        }
     }
 }
